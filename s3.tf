05cabd920b081609558c619731e0eb681f08160f
jahnavikukkapalli <140580697+jahnavikukkapalli@users.noreply.github.com>
2023-09-14 10:15:56 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "s3" {
-bucket         = "moses-dev-terraform-bucket"
+bucket         = "jahav"
 key            = "global/s3/terraform.tfstate"
 region         = "us-east-1"
 dynamodb_table = "terraform-locks"
